46e83a33c690ba617f50e226ebbe708582d026b3
albrecht-spranger <albrecht_spranger@nifty.com>
2025-10-06 15:28:13 +0900

    2025/10/6 15:28

--- cartView.php
+++ cartView.php
@@ -60,7 +60,7 @@ require "head.php";
 				<div class="cartSummary">
 					<div class="cartSummaryText">
 						<p>現在　商品<?= htmlspecialchars((string)count($items), ENT_QUOTES, 'UTF-8') ?>点</p>
-						<p>ご注文小計：税込 <span class="totalPurchase">￥<?= htmlspecialchars(number_format((int)$total), ENT_QUOTES, 'UTF-8') ?><span></p>
+						<p>ご注文小計：税込 <span class="totalPurchase">￥<?= htmlspecialchars(number_format((int)$total), ENT_QUOTES, 'UTF-8') ?></span></p>
 					</div>
 					<form action="purchaseConfirmation.php" method="post">
 						<input type="hidden" name="csrfToken" value="<?= htmlspecialchars($_SESSION['csrfToken'] ?? '', ENT_QUOTES, 'UTF-8'); ?>">
